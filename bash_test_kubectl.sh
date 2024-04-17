a51c09ec92d27453245eae40194105b596ae0a3e
AdelNehili <49352113+AdelNehili@users.noreply.github.com>
2024-04-17 16:02:01 +0200

    Final_Minikube

--- bash_test_kubectl.sh
+++ bash_test_kubectl.sh
@@ -1,37 +1,61 @@
 #!/bin/bash
 
+# Check kubectl Context and Configuration
 # Check if the first argument is "rst" to reset the environment
+
+
+
 if [ "$1" = "rst" ]; then
+    #Minikube reset in case, we got errors
     minikube stop
     minikube delete
     minikube start
 
-fi
+elif [ "$1" = "clear" ]; then
+    #Clear all the .yaml files in the kubernetes
+    kubectl delete deployment my-deployment
+    kubectl delete service my-service
 
-if [ "$1" = "clear" ]; then
-    kubectl delete deployment test-yaml-deployment
-    kubectl delete service test-yaml-service
+    kubectl delete -f aws-deploy.yaml
+    kubectl delete -f aws-service.yaml
 
-    kubectl delete deployment nginx-deployment
-    kubectl delete service nginx-service
-fi
+elif [ "$1" = "kube-context" ]; then
+    #Knowing the kubernetes can be listen by multiple container manager(aws/minikube), we have to be sure to send to the correct one
+    kubectl config get-contexts
+    kubectl config current-context
 
-if [ "$1" = "load" ]; then
+elif [ "$1" = "tunnel" ]; then
+    #Care, this bash command should be runned in another terminal
+    minikube start
+    minikube tunnel
+
+elif [ "$1" = "load" ]; then
+    #Allows to send the image to MINIKUBE
     IMAGE_NAME=test_yaml:latest
     docker build -t $IMAGE_NAME -f ./V1_flask_project/src/Dockerfile ./V1_flask_project/src
     echo "minikube image loading..."
     minikube image load $IMAGE_NAME
-fi
 
-if [ "$1" = "deploy" ]; then
-    kubectl apply -f nginx-deployment.yaml
-    kubectl apply -f nginx-service.yaml
-fi
+elif [ "$1" = "deploy" ]; then
+    #Deploy the kubernete with all the yaml informations
+    kubectl apply -f my-deploy.yaml
+    kubectl apply -f my-service.yaml
 
+
+elif [ "$1" = "display" ]; then
+    #DIsplay the state of deployement/services
+    echo "//__kubectl get deployments"
+    kubectl get deployments
+    echo""
+
+    echo "//__kubectl get services"
+    kubectl get services
+    echo""
+
+    echo "//__kubectl get pod"
+    kubectl get pod
+    echo""
+fi
 
 
 echo "Done."
-# Start Minikube tunnel in the background :
-# Creates a route between your machine and the Minikube cluster, allowing services of type LoadBalancer to be accessed as if they had an external IP
-#minikube start
-#minikube tunnel &
---
+++ install_bash_dir/bash_install_k8s.sh
@@ -0,0 +1,13 @@
+if [ "$1" = "install" ]; then
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+
+    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    chmod +x minikube
+    sudo mv minikube /usr/local/bin/
+    minikube start --driver=docker
+elif [ "$1" = "rst" ]; then
+    minikube start
+    minikube dashboard
+fi
